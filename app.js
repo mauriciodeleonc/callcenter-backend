f458d19285ad7242db7c240ac139e04703d1af6e
Mauricio De Leon <mauricio.deleon.c13@gmail.com>
2020-06-02 23:00:03 -0500

    cors headers test

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 let db;
 
-const allowedOrigins = ["https://asinco.herokuapp.com"];
+const allowedOrigins = ["https://asinco.herokuapp.com", "http://asinco.herokuapp.com"];
 
 app.use(
   cors({
