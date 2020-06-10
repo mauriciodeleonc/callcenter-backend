6f7de4d80665ccc61e4f9ffa04294ef7d5e17acf
Mauricio De Leon <mauricio.deleon.c13@gmail.com>
2020-06-10 15:19:48 -0500

    added allowedOrigins

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(
   cors({
     origin: function (origin, callback) {
       // allow requests with no origin
-      // (like mobile apps or curl requests)      
+      // (like mobile apps or curl requests)    
+      /*  
       if (!origin) return callback(null, true);
       if (allowedOrigins.indexOf(origin) === -1) {
         var msg =
@@ -22,6 +23,7 @@ app.use(
           "allow access from the specified Origin.";
         return callback(new Error(msg), false);
       }
+      */
       return callback(null, true);
     }
   })
